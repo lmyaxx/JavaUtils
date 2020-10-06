07b84737aa9d0d7baa9b0e85f979ea5ed64c3f0f
lmyaxx <1367968283@qq.com>
2020-10-06 09:33:50 +0800

    some utils for future development

--- file-manipulate/src/main/java/file/FileHelper.java
+++ file-manipulate/src/main/java/file/FileHelper.java
@@ -12,19 +12,24 @@ import java.io.*;
  * @Version 1.0
  **/
 public class FileHelper {
-
+    /**
+     *
+     * @param content 要保存的文本的内容
+     * @param filePath 保存文件的路径
+     * @throws IOException IO异常
+     */
     public static void saveAsFileWriter(String content, String filePath) throws IOException {
-        FileWriter fwriter = null;
+        FileWriter fWriter = null;
         try {
             // true表示不覆盖原来的内容，而是加到文件的后面。若要覆盖原来的内容，直接省略这个参数就好
-            fwriter = new FileWriter(filePath, true);
-            fwriter.write(content);
+            fWriter = new FileWriter(filePath, true);
+            fWriter.write(content);
         } catch (IOException ex) {
             ex.printStackTrace();
         } finally {
             try {
-                fwriter.flush();
-                fwriter.close();
+                fWriter.flush();
+                fWriter.close();
             } catch (IOException ex) {
                 ex.printStackTrace();
             }
@@ -36,7 +41,7 @@ public class FileHelper {
         BufferedReader bf = new BufferedReader(new FileReader(file));
         String content = "";
         StringBuilder sb = new StringBuilder();
-        while(content != null){
+        while(true){
             content = bf.readLine();
             if(content == null){
                 break;
